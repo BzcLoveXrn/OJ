ac237deb96ac96637874d3184264ccc882146113
2019500261@qq.com <2019500261@qq.com>
2025-05-23 17:22:18 +0800

    加入建库建表东西

--- oj-backend/sql/create_table.sql
+++ oj-backend/sql/create_table.sql
@@ -47,27 +47,21 @@ create table if not exists question
     index idx_userId (userId)
 ) comment '题目' collate = utf8mb4_unicode_ci;
 
--- 题目提交表
-create table if not exists question_submit
-(
-    id         bigint auto_increment comment 'id' primary key,
-    language   varchar(128)                       not null comment '编程语言',
-    code       text                               not null comment '用户代码',
-    judgeInfo  text                               null comment '判题信息（json 对象）',
-    status     int      default 0                 not null comment '判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）',
-    questionId bigint                             not null comment '题目 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
-    index idx_questionId (questionId),
-    index idx_userId (userId)
-) comment '题目提交';
-
-
-ALTER TABLE question_submit
-    ADD COLUMN judgeMessages TEXT NULL COMMENT '判题详情列表（List<JudgeMessage> 的 JSON 序列化）';
-
+CREATE TABLE IF NOT EXISTS question_submit (
+   id          BIGINT AUTO_INCREMENT COMMENT 'id' PRIMARY KEY,
+   language    VARCHAR(128) NOT NULL COMMENT '编程语言',
+   code        TEXT NOT NULL COMMENT '用户代码',
+   judgeInfo   TEXT NULL COMMENT '判题信息（json 对象）',
+   judgeMessages TEXT NULL COMMENT '判题详情列表（List<JudgeMessage> 的 JSON 序列化）',
+   status      INT DEFAULT 0 NOT NULL COMMENT '判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）',
+   questionId  BIGINT NOT NULL COMMENT '题目 id',
+   userId      BIGINT NOT NULL COMMENT '创建用户 id',
+   createTime  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
+   updateTime  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+   isDelete    TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除',
+   INDEX idx_questionId (questionId),
+   INDEX idx_userId (userId)
+) COMMENT '题目提交';
 
 -- 帖子表
 create table if not exists post
